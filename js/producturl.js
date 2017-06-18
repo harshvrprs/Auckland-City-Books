58f0943f92eec460b783226f11dc8ff300b0b4d4
harshvrprs <parash01@myunitec.ac.nz>
2017-06-18 17:16:26 +1200

    changed

--- js/producturl.js
+++ js/producturl.js
@@ -1,7 +1,7 @@
 
 $.ajax({
     type: 'GET',
-    url: 'https://gist.githubusercontent.com/Ulaganathan1990/29443f1d49825c2cf839db89bd676036/raw/04cb2ec1d0d49904bf9a4128c1c599698c65190c/featured.json',
+    url: 'https://gist.githubusercontent.com/Ulaganathan1990/29443f1d49825c2cf839db89bd676036/raw/6bd4e2a872e7a82898c2b5c579f828e063f0302c/featured.json',
     data: { get_param: 'value' },
     dataType: 'json',
     success: function (data) {
@@ -12,7 +12,7 @@ $.ajax({
 
         //$('.carousel-caption h1').text(value.name);
         //$('.carousel-caption p').text(value.description);
-        $('<div class="item"><img src="'+value.image+'"><div class="carousel-caption"><h1>'+value.name+'</h1><p>'+value.description+'</p> <p><a class="btn btn-lg btn-primary" href="#" role="button">Buy '+value.price+'</a></p></div> </div>').appendTo('.carousel-inner');
+        $('<div class="item"><img src="'+value.image+'"><div class="carousel-caption"><h1>'+value.name+'</h1><p>'+value.description+'</p> <p><button class="btn btn-primary my-cart-btn" data-id="'+value.id+'" data-name="'+value.name+'" data-summary="Book" data-price="'+value.price+'" data-quantity="1" data-image="'+value.image+'" onclick=\"addCart()\">Buy $'+value.price+'</button></p></div> </div>').appendTo('.carousel-inner');
         $('<li data-target="#myCarousel" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
         if(i<data.Featured.length)
           i++;
@@ -28,7 +28,7 @@ $.ajax({
   function openCategory(category) {
 $.ajax({
     type: 'GET',
-    url: 'https://gist.githubusercontent.com/Ulaganathan1990/299fc229e79d505b3e7c6753e40e106b/raw/82ae01978f7ec65995a4308a1c243d6d231a4398/all.json',
+    url: 'https://gist.githubusercontent.com/Ulaganathan1990/299fc229e79d505b3e7c6753e40e106b/raw/be956a220bfe6ed127f21d17776aaaef9c0ac3d8/all.json',
     data: { get_param: 'value' },
     dataType: 'json',
     success: function (data) {
@@ -37,7 +37,7 @@ $.ajax({
       $.each(data.Featured, function(key, item){
 
           if(item.category==category||category=="All")
-          buffer+="<div   class=\"col-md-4\"><img class=\"featurette-image img-responsive center-block\" style=\"height :280px; margin-top:10px;\" src=\""+item.image+"\"+ alt=\"Generic placeholder image\"><h2 style=\"height :35px; overflow: hidden; \" color:black;>"+item.name+"</h2><p  style=\"height :150px;overflow: hidden; \" style=\"font-size:20px\" class=\"lead\">"+item.description+"</p><p><a class=\"btn btn-lg btn-primary\" href=\"#\" role=\"button\">Buy "+item.price+"</a></p></div>";
+          buffer+="<div   class=\"col-md-4\"><img class=\"featurette-image img-responsive center-block\" style=\"height :280px; margin-top:10px;\" src=\""+item.image+"\"+ alt=\"Generic placeholder image\"><h2 style=\"height :35px; overflow: hidden; \" color:black;>"+item.name+"</h2><p  style=\"height :150px;overflow: hidden; \" style=\"font-size:20px\" class=\"lead\">"+item.description+"</p><p><button class=\"btn btn-primary my-cart-btn\" data-id=\""+item.id+"\" data-name=\""+item.name+"\" data-summary=\"Book1\" data-price=\""+item.price+"\" data-quantity=\"1\" data-image=\""+item.image+"\" onclick=\"addCart()\">Buy $"+item.price+"</button></p></div>";
 
         });
         $('.containertab').html(buffer);
@@ -49,5 +49,73 @@ $.ajax({
         // });
 
     }
+});
+}
+
+
+
+$(function () {
+
+  var goToCartIcon = function($addTocartBtn){
+    var $cartIcon = $(".my-cart-icon");
+    //var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
+    //$addTocartBtn.prepend($image);
+    //var position = $cartIcon.position();
+    //$image.animate({
+    //  top: position.top,
+    //  left: position.left
+    //}, 900 , "linear", function() {
+    //  $image.remove();
+    //});
+  }
+
+  $('.my-cart-btn').myCart({
+    currencySymbol: '$',
+    classCartIcon: 'my-cart-icon',
+    classCartBadge: 'my-cart-badge',
+    classProductQuantity: 'my-product-quantity',
+    classProductRemove: 'my-product-remove',
+    classCheckoutCart: 'my-cart-checkout',
+    affixCartIcon: true,
+    showCheckoutModal: true,
+    numberOfDecimals: 2,
+    cartItems: [],
+    afterAddOnCart: function(products, totalPrice, totalQuantity) {
+      console.log("afterAddOnCart", products, totalPrice, totalQuantity);
+    },
+    clickOnCartIcon: function($cartIcon, products, totalPrice, totalQuantity) {
+      console.log("cart icon clicked", $cartIcon, products, totalPrice, totalQuantity);
+    },
+    checkoutCart: function(products, totalPrice, totalQuantity) {
+      console.log("afterAddOnCart","Test"+this.id);
+      var checkoutString = "Total Price: " + totalPrice + "\nTotal Quantity: " + totalQuantity;
+      checkoutString += "\n\n id \t name \t summary \t price \t quantity \t image path";
+      $.each(products, function(){
+        checkoutString += ("\n " + this.id + " \t " + this.name + " \t " + this.summary + " \t " + this.price + " \t " + this.quantity + " \t " + this.image);
+      });
+      alert(checkoutString)
+      console.log("checking out", products, totalPrice, totalQuantity);
+    },
+    getDiscountPrice: function(products, totalPrice, totalQuantity) {
+      console.log("calculating discount", products, totalPrice, totalQuantity);
+      return totalPrice * 0.75;
+    }
+  });
+
+});
+
+function addCart()
+{
+$(function () {
+  var goToCartIcon = function($addTocartBtn){
+  var $cartIcon = $(".my-cart-icon");
+  }
+
+  $('.my-cart-btn').myCart({
+    clickOnAddToCart: function($addTocart){
+      goToCartIcon($addTocart);
+    },
+  });
+
 });
 }
--- js/url.js
+++ js/url.js
@@ -1,6 +1,6 @@
 $.ajax({
     type: 'GET',
-    url: 'https://gist.githubusercontent.com/Ulaganathan1990/29443f1d49825c2cf839db89bd676036/raw/04cb2ec1d0d49904bf9a4128c1c599698c65190c/featured.json',
+    url: 'https://gist.githubusercontent.com/Ulaganathan1990/29443f1d49825c2cf839db89bd676036/raw/6bd4e2a872e7a82898c2b5c579f828e063f0302c/featured.json',
     data: { get_param: 'value' },
     dataType: 'json',
     success: function (data) {
@@ -11,7 +11,7 @@ $.ajax({
 
         //$('.carousel-caption h1').text(value.name);
         //$('.carousel-caption p').text(value.description);
-        $('<div class="item"><img src="'+value.image+'"><div class="carousel-caption"><h1>'+value.name+'</h1><p>'+value.description+'</p> <p><button class="btn btn-primary my-cart-btn" data-id="'+value.id+'" data-name="'+value.name+'" data-summary="Book" data-price="'+value.price+'" data-quantity="1" data-image="'+value.image+'">Buy '+value.price+'</button></p></div> </div>').appendTo('.carousel-inner');
+        $('<div class="item"><img src="'+value.image+'"><div class="carousel-caption"><h1>'+value.name+'</h1><p>'+value.description+'</p> <p><button class="btn btn-primary my-cart-btn" data-id="'+value.id+'" data-name="'+value.name+'" data-summary="'+value.description+'" data-price="'+value.price+'" data-quantity="1" data-image="'+value.image+'" onclick=\"addCart()\">Buy $'+value.price+'</button></p></div> </div>').appendTo('.carousel-inner');
         $('<li data-target="#myCarousel" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
         if(i<data.Featured.length)
           i++;
@@ -27,7 +27,7 @@ $.ajax({
 
 $.ajax({
     type: 'GET',
-    url: 'https://gist.githubusercontent.com/Ulaganathan1990/4cbf9bb2e20cfddc83fa2e9b6e905814/raw/51e77b3bb2fc487468e81ddf32b87120fa216273/deal.json',
+    url: 'https://gist.githubusercontent.com/Ulaganathan1990/4cbf9bb2e20cfddc83fa2e9b6e905814/raw/4e072ed61be5334f274c846fd658dae50b2e7a7b/deal.json',
     data: { get_param: 'value' },
     dataType: 'json',
     success: function (data) {
@@ -38,11 +38,13 @@ $.ajax({
 
         if(i%2==0)
         {
-          buffer+="<hr class=\"featurette-divider\"><div class=\"row featurette\"><div class=\"col-md-7\"><h2 class=\"featurette-heading\">"+item.name+"</h2><p class=\"lead\">"+item.description+"</p><p><button class=\"btn btn-primary my-cart-btn\" data-id=\"1\" data-name=\"Book1\" data-summary=\"Book1\" data-price=\"10\" data-quantity=\"1\" data-image=\"images/art.jpg\">Buy "+item.price+"</button></p></div><div class=\"col-md-5\"><img class=\"featurette-image img-responsive center-block\" src=\""+item.image+"\"+data-src=\"holder.js/500x500/auto\" alt=\"Generic placeholder image\"></div></div>";
+          buffer+="<hr class=\"featurette-divider\"><div class=\"row featurette\"><div class=\"col-md-7\"><h2 class=\"featurette-heading\">"+item.name+"</h2><p class=\"lead\">"+item.description+"</p><button class=\"btn btn-primary my-cart-btn\" data-id=\""+item.id+"\" data-name=\""+item.name+"\" data-summary=\"Book1\" data-price=\""+item.price+"\" data-quantity=\"1\" data-image=\""+item.image+"\" onclick=\"addCart()\">Buy $"+item.price+
+          "</button></div><div class=\"col-md-5\"><img class=\"featurette-image img-responsive center-block\" src=\""+item.image+"\"+data-src=\"holder.js/500x500/auto\" alt=\"Generic placeholder image\"></div></div>";
           i++;
         }
         else {
-          buffer+="<hr class=\"featurette-divider\"><div class=\"row featurette\"><div class=\"col-md-7 col-md-push-5\"><h2 class=\"featurette-heading\">"+item.name+"</h2><p class=\"lead\">"+item.description+"</p><p><a class=\"btn btn-lg btn-primary\" href=\"#\" role=\"button\">Buy "+item.price+"</a></p></div><div class=\"col-md-5 col-md-pull-7\"><img class=\"featurette-image img-responsive center-block\" src=\""+item.image+"\"+data-src=\"holder.js/500x500/auto\" alt=\"Generic placeholder image\"></div></div>";
+          buffer+="<hr class=\"featurette-divider\"><div class=\"row featurette\"><div class=\"col-md-7 col-md-push-5\"><h2 class=\"featurette-heading\">"+item.name+"</h2><p class=\"lead\">"+item.description+"</p><button class=\"btn btn-primary my-cart-btn\" data-id=\""+item.id+"\" data-name=\""+item.name+"\" data-summary=\"Book1\" data-price=\""+item.price+"\" data-quantity=\"1\" data-image=\""+item.image+"\" onclick=\"addCart()\">Buy $"+
+          item.price+"</button></div><div class=\"col-md-5 col-md-pull-7\"><img class=\"featurette-image img-responsive center-block\" src=\""+item.image+"\"+data-src=\"holder.js/500x500/auto\" alt=\"Generic placeholder image\"></div></div>";
           i=0;
         }
         });
@@ -58,19 +60,21 @@ $.ajax({
 });
 
 
+
+
 $(function () {
 
   var goToCartIcon = function($addTocartBtn){
     var $cartIcon = $(".my-cart-icon");
-    var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
-    $addTocartBtn.prepend($image);
-    var position = $cartIcon.position();
-    $image.animate({
-      top: position.top,
-      left: position.left
-    }, 500 , "linear", function() {
-      $image.remove();
-    });
+    //var $image = $('<img width="30px" height="30px" src="' + $addTocartBtn.data("image") + '"/>').css({"position": "fixed", "z-index": "999"});
+    //$addTocartBtn.prepend($image);
+    //var position = $cartIcon.position();
+    //$image.animate({
+    //  top: position.top,
+    //  left: position.left
+    //}, 900 , "linear", function() {
+    //  $image.remove();
+    //});
   }
 
   $('.my-cart-btn').myCart({
@@ -83,14 +87,7 @@ $(function () {
     affixCartIcon: true,
     showCheckoutModal: true,
     numberOfDecimals: 2,
-    cartItems: [
-      {id: 1, name: 'product 1', summary: 'summary 1', price: 10, quantity: 1, image: 'images/img_1.png'},
-      {id: 2, name: 'product 2', summary: 'summary 2', price: 20, quantity: 2, image: 'images/img_2.png'},
-      {id: 3, name: 'product 3', summary: 'summary 3', price: 30, quantity: 1, image: 'images/img_3.png'}
-    ],
-    clickOnAddToCart: function($addTocart){
-      goToCartIcon($addTocart);
-    },
+    cartItems: [],
     afterAddOnCart: function(products, totalPrice, totalQuantity) {
       console.log("afterAddOnCart", products, totalPrice, totalQuantity);
     },
@@ -98,6 +95,7 @@ $(function () {
       console.log("cart icon clicked", $cartIcon, products, totalPrice, totalQuantity);
     },
     checkoutCart: function(products, totalPrice, totalQuantity) {
+      console.log("afterAddOnCart","Test"+this.id);
       var checkoutString = "Total Price: " + totalPrice + "\nTotal Quantity: " + totalQuantity;
       checkoutString += "\n\n id \t name \t summary \t price \t quantity \t image path";
       $.each(products, function(){
@@ -113,3 +111,19 @@ $(function () {
   });
 
 });
+
+function addCart()
+{
+$(function () {
+  var goToCartIcon = function($addTocartBtn){
+  var $cartIcon = $(".my-cart-icon");
+  }
+
+  $('.my-cart-btn').myCart({
+    clickOnAddToCart: function($addTocart){
+      goToCartIcon($addTocart);
+    },
+  });
+
+});
+}
